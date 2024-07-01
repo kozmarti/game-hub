5e7b6d895ab417d518bb93a3797b035d738a46b6
kozmarti <kozmarti@gmail.com>
2024-07-01 20:42:05 +0200

    build sort selector2

--- src/components/GameCardContainer.tsx
+++ src/components/GameCardContainer.tsx
@@ -6,7 +6,7 @@ interface Props {
 }
 const GameCardContainer = ({children}: Props) => {
   return (
-    <Box boxShadow="1px 1px 5px gray" maxW="sm" borderRadius={10} overflow="hidden">{children}</Box>
+    <Box boxShadow="1px 1px 10px #1A202C" maxW="sm" borderRadius={10} overflow="hidden">{children}</Box>
   );
 };
 
