698e78d3a379c45aef348e4d092bf0cc892ae9b7
Marta Kozma <kozmarti@gmail.com>
2025-06-01 19:40:06 +0200

    fetching genres with react query

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -14,7 +14,7 @@ const GenreList = ({onSelectGenre, selectedGenre}: Props) => {
     <>
     <Heading fontSize="2xl" marginBottom={3}>Genres</Heading>
     <List>
-      {data.map((genre) => (
+      {data?.results.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
             <Image
--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,18 @@ import { ChakraProvider, ColorModeScript } from "@chakra-ui/react";
 import App from "./App.tsx";
 import theme from './theme'
 import "./index.css";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
+const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <ChakraProvider theme={theme}>
       <ColorModeScript initialColorMode={theme.config.initialColorMode}/>
-      <App />
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <App />
+      </QueryClientProvider>  
     </ChakraProvider>
   </React.StrictMode>
 );
