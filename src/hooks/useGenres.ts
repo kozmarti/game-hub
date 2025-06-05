637a3158e720804f1461204080310bc3df90f397
Marta Kozma <kozmarti@gmail.com>
2025-06-05 18:06:32 +0200

    red-facto - simplify time calcs

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import genres from "../data/genres";
+import ms from "ms";
 
 const apiClient = new APIClient<Genre>('/genres');
 
@@ -13,7 +14,7 @@ export interface Genre {
 const useGenres = () => useQuery({
     queryKey: ["genres"],
     queryFn: apiClient.getAll,
-    staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    staleTime: ms('24h'),
     initialData: genres
 
 });
--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,6 +1,7 @@
 import { useQuery } from "@tanstack/react-query";
 import APIClient from "../services/api-client";
 import platforms from "../data/platforms";
+import ms from "ms";
 
 const apiClient = new APIClient<Platform>('/platforms/lists/parents');
 
@@ -13,7 +14,7 @@ export interface Platform {
 const usePlatforms = () => useQuery({
     queryKey: ["platforms"],
     queryFn: apiClient.getAll,
-    staleTime: 1000 * 60 * 60 * 24, // 24 hours
+    staleTime: ms('24h'),
     initialData: platforms // Initial data from local file
 
 
