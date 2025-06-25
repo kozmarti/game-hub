738357ea0eaf23e43213c39d61df801e3ca6a522
Marta Kozma <kozmarti@gmail.com>
2025-06-25 22:26:50 +0200

    Refacto - move entity interfaces

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -1,25 +1,13 @@
 import { useInfiniteQuery} from "@tanstack/react-query";
 import { FetchResponse } from "../services/api-client";
-import { Platform } from "./usePlatforms";
 import APIClient from "../services/api-client";
 import ms from "ms";
 import useGameQueryStore from "../store";
+import { Game } from "../entities/Game";
 
 
 const apiClient = new APIClient<Game>('/games');
 
-
-export interface Game {
-    id: number;
-    name: string;
-    slug: string;
-    description_raw: string;
-    background_image: string;
-    parent_platforms: {platform: Platform}[];
-    metacritic: number;
-    rating_top: number;
-  }
-  
 
 const useGames = () => {
   const gameQuery = useGameQueryStore((s => s.gameQuery)); // Ensure gameQuery is initialized
